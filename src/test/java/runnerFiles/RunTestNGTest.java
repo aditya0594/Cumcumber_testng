8be944095f6e9eaaa545eb1328f25d24d6243cac
Amod Mahajan <amomahaj@publicisgroupe.net>
2020-02-12 21:31:46 +0530

    First Commit

---
+++ src/test/java/runnerFiles/RunTestNGTest.java
@@ -0,0 +1,9 @@
+package runnerFiles;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features = "src/test/resources/featureFiles", glue = "stepDefinitions")
+public class RunTestNGTest extends AbstractTestNGCucumberTests {
+
+}
